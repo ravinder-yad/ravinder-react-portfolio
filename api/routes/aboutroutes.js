f3e6db287c4df02ae9ad063c6e462f74418f10fe
Ravinder Yadav <ravinderyadav092007@gmail.com>
2026-02-14 09:11:01 +0530

    Remove unused api directory to fix Vercel Hobby plan function limit

--- api/routes/aboutroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { saveAbout, getAbout } = require("../controllers/aboutcontroller");
-
-const router = express.Router();
-
-router.post("/about", saveAbout);
-router.get("/about", getAbout);
-
-module.exports = router;
